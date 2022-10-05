4bf047f6d49e67c14d85ec1b1491af20a2fcdc94
Алексей Осипов <a.l.e.k.s.e.y.2011@mail.ru>
2022-10-05 16:55:31 +0300

    1

--- keyboards/admin_kb.py
+++ keyboards/admin_kb.py
@@ -4,5 +4,5 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 '''Кнопки клавиатуры админа.'''
 button_load = KeyboardButton('/Загрузить')
 button_delete = KeyboardButton('/Удалить')
-
-button_case_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(button_load).add(button_delete)
+button_menu = KeyboardButton('/Меню')
+button_case_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(button_load).add(button_delete).add(button_menu)
