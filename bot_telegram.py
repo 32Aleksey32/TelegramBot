1a440de874296b55ce299a7fc7587905c9bc0e29
Алексей Осипов <a.l.e.k.s.e.y.2011@mail.ru>
2022-10-05 15:37:31 +0300

    1

--- bot_telegram.py
+++ bot_telegram.py
@@ -10,7 +10,6 @@ async def on_startup(_):
 
 client.register_handlers_client(dp)
 admin.register_handlers_admin(dp)
-other.register_handlers_other(dp)
 
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
